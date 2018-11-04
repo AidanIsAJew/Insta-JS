d661bd4e12e4996e734c45397b7200a9bc6fb3fd
AidanIsAJew <shinqbusiness@gmail.com>
2018-11-03 22:08:21 -0400

    2018-11-03_1008 PM

--- index.js
+++ index.js
@@ -5,22 +5,20 @@ if (Number(process.version.slice(1).split(".")[0]) < 8) throw new Error("Node 8.
 
 // Load up the instgram-private-api library
 const Client = require('instagram-private-api').V1;
+
+// Load our config and logger
+Client.config = require("./config.js");
+Client.logger = require("./modules/Logger.js");
 // We also load the rest of the things we need in this file:
 const Enmap = require("enmap");
 const util = require('util');
 const _ = require('lodash');
 const Promise = require('bluebird');
 
-// We use these vars later on, so maswell load them now
-const device = new Client.Device('bot.NodeJS');
+// We use these lets later on, so maswell load them now
+const device = new Client.Device(Client.config.username);
 const setTimeoutPromise = util.promisify(setTimeout);
-// For some reason (__directory + `./cookies/${username}`)
-// doesn't so we use the full path instead.
-const storage = new Client.CookieFileStorage(__dirname + '/cookies/bot.NodeJS.json');
-
-// Load our config and logger
-Client.config = require("./config.js");
-Client.logger = require("./modules/Logger.js");
+const storage = new Client.CookieFileStorage(__dirname + `/cookies/${Client.config.username}.json`);
 
 // Let's start by getting some useful functions that we'll use throughout
 // the bot.
@@ -31,61 +29,20 @@ require("./modules/functions.js")(Client);
 // This makes things extremely easy for setting dates which we can comapre
 // to later.
 Client.dates = new Enmap({
-    name: "dates"
+  name: "dates"
 });
 
 // And go for login
-Client.Session.create(device, storage, 'bot.NodeJS', 'carter979')
-    .then(async function(session) {
-        // Now you have a session, we can follow / unfollow, anything...
-        // And we want to follow Instagram official profile
-
-        //let usr = await Client.Account.searchForUser(session, 'instagram');
-        //console.log(usr);
-
-        var accountId = '25025320'
-        var feed = new Client.Feed.UserMedia(session, accountId);
-
-        Promise.mapSeries(_.range(0, 20), function() {
-                return feed.get();
-            })
-            .then(function(results) {
-                // result should be Media[][]
-                //Client.logger.log(`NEW:\n\n ${results} \n\n`);
-
-                var media = _.flatten(results);
-                var urls = _.map(media, function(medium) {
-                    Client.logger.log(`\n\n${medium.id}\n\n`);
-                    //console.log(`\n\n####################\n\n\n\n`,medium.id);
-                    return _.last(medium.images);
-                });
-                //Client.logger.log(`NEW:\n\n ${urls} \n\n`);
-            })
+Client.Session.create(device, storage, Client.config.username, Client.config.password)
+  .then(async function(session) {
+    Client.logger.log(`Logged in at ${session.device.username}`, "ready");
+    // Now you have a session, we can follow / unfollow, anything...
+    // And we want to follow Instagram official profile
 
 
-        /*var find = await Client.findHashtag(session, 'follow4follows');
-        var sort = await find.find(x => x.params.name === 'follow4follows');
-        console.log(sort);*/
+    //60 × 60 seconds = 3600 seconds = 3600 × 1000 milliseconds = 3,600,000 ms + 10 (just in case...).
+    const time = (60 * 60 * 1000) + 10
+    Client.everyHour(session);
+    setInterval(Client.everyHour, time, session);
 
-
-        /*var find = await Client.findRelationship(session, 'instagram');
-        if (find.params.following === true) {
-          var resolve = await Client.findUnfollow(session, 'instagram');
-          console.log(resolve.params);
-        } else {
-          var resolve = await Client.findFollow(session, 'instagram');
-          console.log(resolve.params);
-        }*/
-    });
-
-
-Client.dates.defer.then(() => {
-    Client.logger.log(Client.dates.size + " keys loaded");
-
-    Client.dates.set("lastDate", Date.now());
-
-    setTimeoutPromise(240).then(() => {
-        var dat = (Date.now() - Client.dates.get("lastDate"));
-        Client.logger.log(dat);
-    });
-});
+  });
--- modules/functions.js
+++ modules/functions.js
@@ -1,40 +1,93 @@
 module.exports = (Client) => {
-    /***************************************************/
-    /* Client.Relationship.create(session, account.id) */
-    /*  Follows the "account.id" 's profile            */
-    /*                                                 */
-    /* Client.Relationship.destroy(session, account.id)*/
-    /*  UnFollows the "account.id" 's profile          */
-    /***************************************************/
+  /***************************************************/
+  /* Client.Relationship.create(session, account.id) */
+  /*  Follows the "account.id" 's profile            */
+  /*                                                 */
+  /* Client.Relationship.destroy(session, account.id)*/
+  /*  UnFollows the "account.id"
+'s profile          */
+/***************************************************/
 
-    Client.findFollow = async (session, name) => {
-        const account = await Client.Account.searchForUser(session, name);
-        var get = await Client.Relationship.get(session, account.id);
-        if (get.params.following === false) {
-            return Client.Relationship.create(session, account.id);
-        } else {
-            return `You are already following ${name}.`;
-        }
-    };
+  Client.findFollow = async (session, name) => {
+    const account = await Client.Account.searchForUser(session, name);
+    var get = await Client.Relationship.get(session, account.id);
+    if (get.params.following === false) {
+      return Client.Relationship.create(session, account.id);
+    } else {
+      return `You are already following ${name}.`;
+    }
+  };
 
-    Client.findUnfollow = async (session, name) => {
-        const account = await Client.Account.searchForUser(session, name);
-        var get = await Client.Relationship.get(session, account.id);
-        if (get.params.following === true) {
-            return Client.Relationship.destroy(session, account.id);
-        } else {
-            return `You are already not following ${name}.`;
-        }
-    };
+  Client.findUnfollow = async (session, name) => {
+    const account = await Client.Account.searchForUser(session, name);
+    var get = await Client.Relationship.get(session, account.id);
+    if (get.params.following === true) {
+      return Client.Relationship.destroy(session, account.id);
+    } else {
+      return `You are already not following ${name}.`;
+    }
+  };
 
-    Client.findRelationship = async (session, name) => {
-        const account = await Client.Account.searchForUser(session, name);
-        var get = await Client.Relationship.get(session, account.id);
-        return get;
-    };
+  Client.findRelationship = async (session, name) => {
+    const account = await Client.Account.searchForUser(session, name);
+    var get = await Client.Relationship.get(session, account.id);
+    return get;
+  };
 
-    Client.findHashtag = async (session, name) => {
-        const tag = await Client.Hashtag.search(session, name);
-        return tag;
-    };
+  Client.findHashtag = async (session, name) => {
+    const tag = await Client.Hashtag.search(session, name);
+    return tag;
+  };
+
+
+  Client.waitMin = async (num) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, num * 1000 * 60);
+    });
+  };
+
+  Client.everyHour = async (session) => {
+
+    const _ = require('lodash');
+    const Promise = require('bluebird');
+
+    Client.logger.log(`Fetching posts tagged with #${Client.config.tag}`)
+    let feed = new Client.Feed.TaggedMedia(session, Client.config.tag, 10);
+    let ids = [];
+
+    await Promise.mapSeries(_.range(0, 20), function() {
+        return feed.get();
+      })
+      .then(async function(results) {
+        // result should be Media[][]
+        let media = _.flatten(results);
+        let urls = _.map(media, function(medium) {
+          if (Client.config.debug === true) {
+            Client.logger.log(`${medium.id}`, "debug");
+          }
+          ids.push(medium.id);
+        });
+      });
+
+    const min = 2;
+    Client.logger.log(`Done! Fetched ${ids.length - 1} posts.`);
+    Client.logger.log(`Liking each posts with ${min} min interval between. Rate: ${60/min}/hr`)
+    for (let i = 0; i < ids.length; i++) {
+      if (Client.config.debug === true) {
+        Client.logger.log(`Catching`, "debug");
+      }
+
+      await Client.waitMin(min);
+
+      if (Client.config.debug === true) {
+        Client.logger.log(`Resolved`, "debug");
+      }
+
+      await Client.Like.create(session, ids[i]);
+      const media = await Client.Media.getById(session, ids[i]);
+      Client.logger.log(`Liked post by ${media.account.params.username}`);
+    }
+    Client.logger.log(`Done!`);  };
 };
